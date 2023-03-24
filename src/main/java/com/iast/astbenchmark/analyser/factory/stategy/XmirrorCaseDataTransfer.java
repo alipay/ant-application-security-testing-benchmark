526624fd55e523f72d98e6bc1670f9fcc48e09ec
mengmeng.meng <kan.yk@antgroup.com>
2023-03-24 19:12:44 +0800

    xmirror

---
+++ src/main/java/com/iast/astbenchmark/analyser/factory/stategy/XmirrorCaseDataTransfer.java
@@ -0,0 +1,103 @@
+package com.iast.astbenchmark.analyser.factory.stategy;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.iast.astbenchmark.analyser.bean.BaseOriginalDataBean;
+import com.iast.astbenchmark.analyser.bean.CaseDataCollectResultBean;
+import com.iast.astbenchmark.analyser.bean.consts.VendorEnum;
+import com.iast.astbenchmark.analyser.factory.CaseDataTransfer;
+import com.iast.astbenchmark.analyser.factory.stategy.dongtai.DongResultBean;
+import com.iast.astbenchmark.analyser.factory.stategy.dongtai.DongTaintItemBean;
+import com.iast.astbenchmark.analyser.factory.stategy.xmirror.XmirrorRecord;
+import com.iast.astbenchmark.analyser.factory.stategy.xmirror.XmirrorRootBean;
+import com.iast.astbenchmark.analyser.service.ConfigService;
+import com.iast.astbenchmark.analyser.util.CaseResultutils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class XmirrorCaseDataTransfer implements CaseDataTransfer {
+    @Autowired
+    private ConfigService configService;
+
+    @Override
+    public VendorEnum vendor() {
+        return VendorEnum.XMIRROR;
+    }
+
+    @Override
+    public CaseDataCollectResultBean doOperation() {
+        Long time = System.currentTimeMillis();
+        CaseDataCollectResultBean resultBean = new CaseDataCollectResultBean();
+        resultBean.setVendor(vendor());
+        resultBean.setReportId(this.vendor().getDescription() + "@" + time);
+        resultBean.setCaseTime(time);
+        /**
+         *  Step1 ->获取检出结果并解析；
+         *  指定检测结果目录 以及检测标记
+         */
+        List<XmirrorRecord> taintItemBeans = getReportLogArray(configService.getDetection(this.vendor()));
+        /**
+         *  Step2 -> 抽取Tag
+         *  默认使用MethedName作为Case的tag进行标记
+         */
+        Map<String, BaseOriginalDataBean> tagMap = convertToTagMap(taintItemBeans);
+        resultBean.setCaseDetectionItems(CaseResultutils.caseAnalyse(tagMap));
+        return resultBean;
+    }
+
+    private Map<String, BaseOriginalDataBean> convertToTagMap(List<XmirrorRecord> logsBeans) {
+        if (CollectionUtils.isEmpty(logsBeans)) {
+            return Maps.newHashMap();
+        }
+        return logsBeans.stream().filter(e -> e.getSecurityLevelId()>=2
+                        && StrUtil.isNotEmpty(e.getIastParam()) && StrUtil.isNotEmpty(e.getVulUrl()))
+                .collect(Collectors.toMap(e1 -> getTagKey(e1), e2 -> e2, (k1, k2) -> k1));
+    }
+
+    private String getTagKey(XmirrorRecord baseData) {
+        String url = baseData.getVulUrl();
+        if (url.contains("case00")) {
+            url =  url.split("\\?")[0];
+            String tag = "aTaintCase00" + url.split("case00")[1].split("/")[0];
+            if (url.endsWith("/2")) {
+                tag = tag + "_2";
+            } else if (url.endsWith("/1")) {
+                tag = tag + "_1";
+            } else if (url.endsWith("/3")) {
+                tag = tag + "_3";
+            } else if (url.endsWith("/4")) {
+                tag = tag + "_4";
+            } else if (url.endsWith("/5")) {
+                tag = tag + "_5";
+            } else if (url.endsWith("/6")) {
+                tag = tag + "_6";
+            } else if (url.endsWith("/7")) {
+                tag = tag + "_7";
+            }
+            return tag;
+        }
+        return "";
+    }
+
+    private List<XmirrorRecord> getReportLogArray(String path) {
+        List<XmirrorRecord> itemBeans = Lists.newArrayList();
+        JSONArray jsonArray =JSONUtil.readJSONArray(FileUtil.file(path), Charset.forName("utf-8"));
+        for (Object o : jsonArray) {
+            XmirrorRootBean resultBean = JSONUtil.toBean(JSONUtil.toJsonStr(o), XmirrorRootBean.class);
+            itemBeans.addAll(resultBean.getData().getRecords());
+        }
+        return itemBeans;
+    }
+}
