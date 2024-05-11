e745ce0c53df9e66b403a170c111f1b868b3d0d4
AntJiuFo <131324870+AntJiuFo@users.noreply.github.com>
2024-05-11 16:40:30 +0800

    sast 目录变更&注释补全 (#63)

---
+++ sast-java/src/main/java/com/sast/astbenchmark/cases/completeness/async/example_001_T.java
@@ -0,0 +1,5 @@
+package com.sast.astbenchmark.cases.completeness.async;
+
+
+public class example_001_T {
+}
---
+++ sast-java/src/main/java/com/sast/astbenchmark/cases/completeness/base/object/javaNative/Base_StringBuffer_001_T.java
@@ -0,0 +1,44 @@
+package com.sast.astbenchmark.cases.completeness.base.object.javaNative;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Introduction 对象中的简单类型对象，StringBuffer对象为污点
+ * Level X
+ * Date 2024-05-09
+ */
+// assession information start
+// real vulnerability = true
+// assession project = 完整度->基础跟踪能力->污点对象完整度->java原生对象->StringBuffer
+// compose = Base_StringBuffer_001_T.java
+// bind_url = completeness/base/object/javaNative/Base_StringBuffer_001_T
+// assession information end
+
+@RestController()
+@RequestMapping("completeness/base/object/javaNative")
+public class Base_StringBuffer_001_T {
+    @PostMapping("Base_StringBuffer_001_T")
+    public Map<String, Object> aTaintCase0153(@RequestBody String cmd) {
+        Map<String, Object> modelMap = new HashMap<>();
+        if (cmd == null) {
+            modelMap.put("status", "error");
+            return modelMap;
+        }
+        StringBuffer data = new StringBuffer();
+        data.append(cmd);
+        try {
+            Runtime.getRuntime().exec(data.toString());
+            modelMap.put("status", "success");
+        } catch (IOException e) {
+            modelMap.put("status", "error");
+        }
+        return modelMap;
+    }
+}
