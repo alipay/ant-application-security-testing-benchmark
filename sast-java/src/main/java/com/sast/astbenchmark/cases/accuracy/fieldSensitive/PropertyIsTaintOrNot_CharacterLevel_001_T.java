35d165ab1bb85e7f368e8fd1b3761c83d66563cf
九佛 <shishaosen.sss@antgroup.com>
2024-05-20 14:39:36 +0800

    变更一些case

--- sast-java/src/main/java/com/sast/astbenchmark/cases/accuracy/fieldSensitive/PropertyIsTaintOrNot_CharacterLevel_001_T.java
+++ sast-java/src/main/java/com/sast/astbenchmark/cases/accuracy/fieldSensitive/PropertyIsTaintOrNot_CharacterLevel_001_T.java
@@ -16,21 +16,19 @@ import java.util.Map;
  */
 // assession information start
 // real vulnerability = true
-// assession project = 准确度->域敏感->对象部分属性为污点->字符串
-// compose = !PropertyIsTaintOrNot_String_002_F.java && PropertyIsTaintOrNot_String_001_T.java
-// bind_url = completeness/base/chain/astTaint/PropertyIsTaintOrNot_String_002_F
+// assession project = 准确度->域敏感->对象部分属性为污点->字符级敏感
+// compose = !PropertyIsTaintOrNot_CharacterLevel_002_F.java && PropertyIsTaintOrNot_CharacterLevel_001_T.java
+// bind_url = completeness/base/chain/astTaint/PropertyIsTaintOrNot_CharacterLevel_001_T
 // assession information end
 @RestController()
 @RequestMapping("accuracy/fieldSensitive/propertyIsTaintOrNot")
-public class PropertyIsTaintOrNot_String_002_F {
-    @PostMapping(value = "PropertyIsTaintOrNot_String_002_F-2")
-    public Map<String, Object> aTaintCase023_2(@RequestParam String param) {
+public class PropertyIsTaintOrNot_CharacterLevel_001_T {
+    @PostMapping(value = "PropertyIsTaintOrNot_CharacterLevel_001_T")
+    public Map<String, Object> aTaintCase023(@RequestParam String domain) {
         Map<String, Object> modelMap = new HashMap<>();
         try {
-            String domain = "www.fixdomain.com/a";
-            String url = domain+param;
-            String not_taint = url.substring(0,17);
-            HttpUtil.doGet(not_taint);
+            String param = "?a=1";
+            HttpUtil.doGet(domain+param);
             modelMap.put("status", "success");
         } catch (Exception e) {
             modelMap.put("status", "error");
