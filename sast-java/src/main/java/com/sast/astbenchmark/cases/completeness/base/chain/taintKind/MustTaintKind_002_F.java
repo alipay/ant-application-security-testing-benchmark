7220f0b8d867d63f210b93c2121c6009224d70c1
Feysh <99711777+Feysh-Inc@users.noreply.github.com>
2024-06-04 16:37:39 +0800

    testcase: add some testcases for java sast engine analysis capability (#80)
    * feat(testcase): add some testcases for java sast engine analysis capability
    
    - Issue: #79
    - taintKind
    - aliasAnalysis
    - pathSensitive
    - contextSensitive
    
    * fix(testcase): fix some class name and comment errors
    
    - Issue: #79
    - taintKind
    - aliasAnalysis
    - pathSensitive
    - contextSensitive
    
    * fix(testcase): fix assession project error of comment
    
    - Issue: #79
    - taintKind
    - pathSensitive
    
    * fix(testcase): fix some assession project and description errors of comment
    
    - Issue: #79
    - taintKind
    - pathSensitive
    
    * fix(testcase): fix some assession project and description errors of comment
    
    - Issue: #79
    - pathSensitive

---
+++ sast-java/src/main/java/com/sast/astbenchmark/cases/completeness/base/chain/taintKind/MustTaintKind_002_F.java
@@ -0,0 +1,26 @@
+package com.sast.astbenchmark.cases.completeness.base.chain.taintKind;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Introduction 污点链路样本中的污点分类，一定被source污染，且没有sink
+ * Level X
+ * Date 2024-05-23
+ */
+// assession information start
+// real vulnerability = false
+// assession project = 完整度->基础跟踪能力->污点链路完整度->污点状态->一定被source污染
+// compose = MustTaintKind_001_T.java && !MustTaintKind_002_F.java && !MustTaintKind_003_F.java
+// bind_url = completeness/base/chain/taintKind/MustTaintKind_002_F
+// assession information end
+@RestController
+@RequestMapping("completeness/base/chain/taintKind")
+public class MustTaintKind_002_F {
+    @GetMapping("MustTaintKind_002_F")
+    public void testcase(@RequestParam String name) {
+        String sql = "select * from user where username = " + name;
+    }
+}
