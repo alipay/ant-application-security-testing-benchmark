2bf606e7768911a83f6e7b7cf77cf26bd0b70217
curryooo <118245688+curryooo@users.noreply.github.com>
2025-07-01 16:30:14 +0800

    Curry0624 all (#137)
    * sast-go/js/python2/python3 case update &config.json update
    
    * sast-go/js/python2/python3 case update &config.json update
    
    * sast-go/js/python2/python3 case update &config.json update
    
    * sast-go/js/python2/python3 case update &config.json update& java run
    
    * py2/3 caseupdate argument_passing_various_types

--- sast-python2/case/completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_005_T.py
+++ sast-python2/case/completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_005_T.py
@@ -2,17 +2,20 @@
 # evaluation information start
 # real case = true
 # evaluation item = 完整度->单应用跟踪完整度->函数和方法调用->参数传递
-# scene introduction = 参数列表
+# scene introduction = 参数列表2
 # level = 2
-# bind_url = completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_001_T
+# bind_url = completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_005_T
 # evaluation information end
 
 import os
 
 
-def argument_passing_various_types_001_T(taint_src):
+def argument_passing_various_types_005_T(taint_src):
     def process(a, b=2, *args, **kwargs):
-        taint_sink(b)
+        if 'd' in kwargs: d = kwargs['d']; del kwargs['d']
+        else: d = 3
+        c = kwargs['c']; del kwargs['c']
+        taint_sink(kwargs['data'])
 
     # 调用过程，展示各种参数的使用方式
     process(1, taint_src, 4, c=u"critical", data=taint_src)
@@ -24,4 +27,4 @@ def taint_sink(o):
 
 if __name__ == u"__main__":
     taint_src = u"taint_src_value"
-    argument_passing_various_types_001_T(taint_src)
+    argument_passing_various_types_005_T(taint_src)
--- sast-python2/case/completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_007_T.py
+++ sast-python2/case/completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_007_T.py
@@ -2,20 +2,20 @@
 # evaluation information start
 # real case = true
 # evaluation item = 完整度->单应用跟踪完整度->函数和方法调用->参数传递
-# scene introduction = 参数列表
+# scene introduction = 参数列表3
 # level = 2
-# bind_url = completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_001_T
+# bind_url = completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_007_T
 # evaluation information end
 
 import os
 
 
-def argument_passing_various_types_001_T(taint_src):
+def argument_passing_various_types_007_T(taint_src):
     def process(a, b=2, *args, **kwargs):
-        taint_sink(b)
+        taint_sink(args[0])
 
     # 调用过程，展示各种参数的使用方式
-    process(1, taint_src, 4, c=u"critical", data=taint_src)
+    process(1, taint_src, c=u"critical", data=taint_src, *(taint_src,"nono"))
 
 
 def taint_sink(o):
@@ -24,4 +24,4 @@ def taint_sink(o):
 
 if __name__ == u"__main__":
     taint_src = u"taint_src_value"
-    argument_passing_various_types_001_T(taint_src)
+    argument_passing_various_types_007_T(taint_src)
