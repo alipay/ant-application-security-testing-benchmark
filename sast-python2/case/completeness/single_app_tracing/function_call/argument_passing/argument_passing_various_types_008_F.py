ab4346efa828600fd179260593d2a469bc9c1b30
小藓 <mengmeng.meng@digital-engine.com>
2025-07-01 15:32:41 +0800

    py2/3 caseupdate argument_passing_various_types

--- sast-python2/case/completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_008_F.py
+++ sast-python2/case/completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_008_F.py
@@ -4,13 +4,13 @@
 # evaluation item = 完整度->单应用跟踪完整度->函数和方法调用->参数传递
 # scene introduction = 参数列表3
 # level = 2
-# bind_url = completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_006_F
+# bind_url = completeness/single_app_tracing/function_call/argument_passing/argument_passing_various_types_008_F
 # evaluation information end
 
 import os
 
 
-def argument_passing_various_types_006_F(taint_src):
+def argument_passing_various_types_008_F(taint_src):
     def process(a, b=2, *args, **kwargs):
         taint_sink(args[1])
 
@@ -24,4 +24,4 @@ def taint_sink(o):
 
 if __name__ == u"__main__":
     taint_src = u"taint_src_value"
-    argument_passing_various_types_006_F(taint_src)
+    argument_passing_various_types_008_F(taint_src)
