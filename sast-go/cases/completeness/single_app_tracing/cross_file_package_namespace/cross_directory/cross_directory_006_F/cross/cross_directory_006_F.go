d24bda3276be7ce898a690e56fbb1ddf306f08ae
curryooo <118245688+curryooo@users.noreply.github.com>
2025-07-17 16:00:59 +0800

    sast -go case update (#138)
    * sast-go/js/python2/python3 case update &config.json update
    
    * sast-go/js/python2/python3 case update &config.json update
    
    * sast-go/js/python2/python3 case update &config.json update
    
    * sast-go/js/python2/python3 case update &config.json update& java run
    
    * py2/3 caseupdate argument_passing_various_types
    
    * py2/3 caseupdate argument_passing_various_types
    
    * go caseUpdate
    
    * go caseUpdate crossdir
    
    * go caseUpdate crossdir json fromat

---
+++ sast-go/cases/completeness/single_app_tracing/cross_file_package_namespace/cross_directory/cross_directory_006_F/cross/cross_directory_006_F.go
@@ -0,0 +1,15 @@
+package main
+
+import "cross_directory_006_F/cross/cross_01/cross_02"
+
+// evaluation information start
+// real case = true
+// evaluation item = 完整度->单应用跟踪完整度->文件、包、命名空间->跨包
+// scene introduction = 跨package
+// level = 2
+// bind_url = completeness/single_app_tracing/cross_file_package_namespace/cross_directory/cross_directory_006_F/cross/cross_directory_006_F
+// evaluation information end
+
+func Cross_directory_006_F(__taint_src string) {
+	cross_pkg_deep2.Cross_directory_006_F_a(__taint_src)
+}
---
+++ sast-go/cases/completeness/single_app_tracing/cross_file_package_namespace/cross_directory/cross_directory_008_F/cross/go.mod
@@ -0,0 +1,3 @@
+module cross_directory_008_F
+
+go 1.24.4
