33c7f404e87697a31be6ce28f402bfb9a77c5438
mengmeng.meng <mengmeng.meng@digital-engine.com>
2025-09-30 21:49:16 +0800

    go case update 7

--- sast-go/cases/completeness/single_app_tracing/function_call/return_value_passing/named_return_004_T/named_return_004_T.go
+++ sast-go/cases/completeness/single_app_tracing/function_call/return_value_passing/named_return_004_T/named_return_004_T.go
@@ -1,4 +1,3 @@
-
 // evaluation information start
 // real case = true
 // evaluation item = 完整度->单应用跟踪完整度->函数和方法调用->返回值传递
@@ -8,6 +7,7 @@
 // evaluation information end
 
 package main
+
 import "os/exec"
 
 func named_return_004_T(__taint_src interface{}) {
@@ -19,14 +19,14 @@ func named_return_004_T(__taint_src interface{}) {
 
 func processData(s interface{}, i interface{}) (ret interface{}) {
 	ret = "_"
-	return s // 主要区别位于这里，在具名返回值的情况下 裸返回return默认返回ret。但uast4Go在处理具名返回值时存在bug，导致此处的return s被覆盖成return ret
+	return s
 }
 
 func __taint_sink(o interface{}) {
 	_ = exec.Command("sh", "-c", o.(string)).Run()
-	}
+}
 
 func main() {
-    __taint_src := "taint_src_value"
-    named_return_004_T(__taint_src)
+	__taint_src := "taint_src_value"
+	named_return_004_T(__taint_src)
 }
