fdc9c5976ec0c34e215079c67d608f1ada324e27
幻华 <xueyunrui.xyr@digital-engine.com>
2025-12-09 18:25:33 +0800

    feat: sast-java 箭头函数

---
+++ sast-java/src/main/java/com/sast/astbenchmark/case_language_maturity/completeness/single_app_tracing/function_call/arrow_function/Arrow_Function_001_T.java
@@ -0,0 +1,32 @@
+package com.sast.astbenchmark.case_language_maturity.completeness.single_app_tracing.function_call.arrow_function;
+
+import com.sast.astbenchmark.common.utils.SinkUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+// evaluation information start
+// real case = true
+// evaluation item = 完整度->单应用跟踪完整度->函数和方法调用->参数传递
+// scene introduction = 参数传递->单个参数
+// level = 2
+// bind_url = completeness/single_app_tracing/function_call/arrow_function/Argument_Passing_001_T
+// date = 2025-12-09 14:12:00
+// evaluation information end
+
+@RestController()
+@RequestMapping("completeness/single_app_tracing/function_call/arrow_function")
+public class Arrow_Function_001_T {
+  @GetMapping("Arrow_Function_001_T/{cmd}")
+  public Map<String, Object> arrow_function_001_t(@PathVariable String cmd) {
+    Map<String, Object> modelMap = new HashMap<>();
+    Function<String, String> function = (String s) -> {
+      return s;
+    };
+    SinkUtil.sink(function.apply(cmd));
+    modelMap.put("status", "success");
+    return modelMap;
+  }
+}
