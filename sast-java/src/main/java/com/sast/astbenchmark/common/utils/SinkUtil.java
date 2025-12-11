51209f0f6a6ea393978cc015815c0c92786773c1
幻华 <xueyunrui.xyr@digital-engine.com>
2025-12-11 15:29:42 +0800

    perf: 针对需求调整工具

--- sast-java/src/main/java/com/sast/astbenchmark/common/utils/SinkUtil.java
+++ sast-java/src/main/java/com/sast/astbenchmark/common/utils/SinkUtil.java
@@ -1,7 +1,16 @@
 package com.sast.astbenchmark.common.utils;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class SinkUtil {
-    public static void sink(Object param) {
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  public static void sink(Object param) {
+    try {
+      System.out.println(mapper.writeValueAsString(param));
+    } catch (Exception e) {
+      throw new RuntimeException("JSON 序列化失败", e);
     }
+  }
 }
