81353a469668a31b4e8bcb7bbbbb23068db1d031
幻华 <xueyunrui.xyr@digital-engine.com>
2025-12-22 18:43:03 +0800

    feat:sast-js 子类对象

---
+++ sast-js/case/completeness/single_app_tracing/interface_class/subclass/method_override_002_F.js
@@ -0,0 +1,35 @@
+// evaluation information start
+// real case = false
+// evaluation item = 完整度->单应用跟踪完整度->接口与类->子类对象
+// scene introduction = 子类覆盖
+// level = 2
+// bind_url = completeness/single_app_tracing/interface_class/subclass/method_override_002_F
+// date = 2025-12-17 08:20:50
+// evaluation information end
+const { execSync } = require('child_process');
+
+function method_override_002_F(__taint_src) {
+  class Parent {
+    getData() {
+      return __taint_src;
+    }
+  }
+
+  class Child extends Parent {
+    // 场景特点：子类重写父类方法但不调用父类方法，返回非污点数据
+    getData() {
+      return "safe_data";
+    }
+  }
+
+  const child = new Child();
+  const result = child.getData();
+  __taint_sink(result);
+}
+
+function __taint_sink(o) {
+  execSync(o);
+}
+
+const taint_src = "taint_src_value";
+method_override_002_F(taint_src);
