b619f7953af1eb22dc1fc72a35b7e7dff8344e24
幻华 <xueyunrui.xyr@digital-engine.com>
2025-12-26 14:22:42 +0800

    feat: sast-python3 异常抛出和捕获

--- sast-python3/case/accuracy/path_sensitive/exception_throw/exception_throw_004_F.py
+++ sast-python3/case/accuracy/path_sensitive/exception_throw/exception_throw_004_F.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 # evaluation information start
-# real case = false
+# real case = true
 # evaluation item = 准确度->路径敏感分析->异常抛出和捕获
-# scene introduction = 异常抛出-try块
+# scene introduction = 异常抛出-except块
 # level = 3
-# bind_url = accuracy/path_sensitive/exception_throw/exception_throw_002_F
+# bind_url = accuracy/path_sensitive/exception_throw/exception_throw_004_F
 # evaluation information end
 import os
 
 
-def exception_throw_002_F(taint_src):
+def exception_throw_004_F(taint_src):
     try:
-        taint_sink("safe_value")
-        raise Exception("_")
+        raise Exception("safe_value")
     except Exception as e:
-        pass
+        taint_sink(e)
 
 
 def taint_sink(o):
-    os.system(o)
+    os.system(str(o))
 
 
 # 示例调用
 if __name__ == "__main__":
     taint_src = "taint_src_value"
-    exception_throw_002_F(taint_src)
+    exception_throw_004_F(taint_src)
