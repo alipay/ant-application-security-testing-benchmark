5ee1a74c9d8b4570aceb6567b52f5ea362fb6ea6
幻华 <xueyunrui.xyr@digital-engine.com>
2025-12-26 14:22:48 +0800

    feat: sast-python3 异常抛出与捕获

--- sast-python3/case/completeness/single_app_tracing/exception_error/exception_throw/exception_try_except_star_002_F.py
+++ sast-python3/case/completeness/single_app_tracing/exception_error/exception_throw/exception_try_except_star_002_F.py
@@ -8,9 +8,9 @@
 # evaluation information end
 
 
-#Python 3.11引入的语法，如果运行环境低于Python 3.11将会报错。不需要的话可以删掉
 import os
-from exceptiongroup import ExceptionGroup
+# Python 3.11引入的语法，如果运行环境低于Python 3.11将会报错。不需要的话可以删掉
+# from exceptiongroup import ExceptionGroup
 
 
 def exception_try_except_star_002_F(taint_src):
