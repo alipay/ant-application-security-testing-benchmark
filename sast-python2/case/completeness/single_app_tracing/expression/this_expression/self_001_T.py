a03bab4f59647744f0ab42b70f9bb90b916b1aab
幻华 <xueyunrui.xyr@digital-engine.com>
2026-01-14 14:07:41 +0800

    feat: sast-python2 this 表达式

---
+++ sast-python2/case/completeness/single_app_tracing/expression/this_expression/self_001_T.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# evaluation information start
+# real case = true
+# evaluation item = 完整度->单应用跟踪完整度->表达式->this表达式
+# scene introduction = self
+# level = 2
+# bind_url = completeness/single_app_tracing/expression/this_expression/self_001_T
+# evaluation information end
+import os
+
+
+class Base(object):
+    def __init__(self, data):
+        self.data = data
+
+
+def self_001_T(taint_src):
+    obj = Base(taint_src)
+    taint_sink(obj.data)
+
+
+def taint_sink(o):
+    os.system(o)
+
+
+if __name__ == u"__main__":
+    taint_src = u"taint_src_value"
+    self_001_T(taint_src)
