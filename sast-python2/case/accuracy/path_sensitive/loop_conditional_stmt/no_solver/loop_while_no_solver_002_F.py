e01ce9c25f051012bbe104e1c20033f9728fd107
幻华 <xueyunrui.xyr@digital-engine.com>
2026-01-14 14:10:29 +0800

    feat: sast-python2 条件语句、条件表达式和循环结构-路径敏感分析

--- sast-python2/case/accuracy/path_sensitive/loop_conditional_stmt/no_solver/loop_while_no_solver_002_F.py
+++ sast-python2/case/accuracy/path_sensitive/loop_conditional_stmt/no_solver/loop_while_no_solver_002_F.py
@@ -2,21 +2,18 @@
 # evaluation information start
 # real case = false
 # evaluation item = 准确度->路径敏感分析->条件语句、条件表达式和循环结构->无需通过对不同的条件进行求解，即能够区分不同的执行路径的状态
-# scene introduction = 区分if else准入条件（不需求解）->if->区分分支
+# scene introduction = while循环
 # level = 3
-# bind_url = accuracy/path_sensitive/loop_conditional_stmt/no_solver/conditional_if_no_solver_002_F
+# bind_url = accuracy/path_sensitive/loop_conditional_stmt/no_solver/loop_while_no_solver_002_F
 # evaluation information end
 import os
 
 
-def conditional_if_no_solver_002_F(taint_src):
-    result = ""
-    if True:
-        result = "safe_value"
-    else:
-        result = taint_src
-
-    taint_sink(result)
+def loop_while_no_solver_002_F(taint_src):
+    i = 0
+    while i < 3:
+        taint_sink("safe_value")
+        i = i + 1
 
 
 def taint_sink(o):
@@ -26,4 +23,4 @@ def taint_sink(o):
 # 示例调用
 if __name__ == u"__main__":
     taint_src = u"taint_src_value"
-    conditional_if_no_solver_002_F(taint_src)
+    loop_while_no_solver_002_F(taint_src)
