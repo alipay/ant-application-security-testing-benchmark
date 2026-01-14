15c2857fc3361e4724e4801b21b8d281c2d4c7a7
幻华 <xueyunrui.xyr@digital-engine.com>
2026-01-14 14:08:09 +0800

    feat: sast-python2 区分不同类对象的不同字段

--- sast-python2/case/accuracy/field_sensitive/class/field_len_002_F.py
+++ sast-python2/case/accuracy/field_sensitive/class/field_len_002_F.py
@@ -2,7 +2,7 @@
 # evaluation information start
 # real case = false
 # evaluation item = 准确度->对象敏感与域敏感分析->区分不同类对象的不同字段
-# scene introduction = 路径长度
+# scene introduction = 路径长度1
 # level = 3
 # bind_url = accuracy/field_sensitive/class/field_len_002_F
 # evaluation information end
