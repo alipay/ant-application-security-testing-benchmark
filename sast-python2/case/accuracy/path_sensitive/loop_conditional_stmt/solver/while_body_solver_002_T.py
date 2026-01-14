44e3e0e0f03128051f1de2eaaf40753a9d9be023
幻华 <xueyunrui.xyr@digital-engine.com>
2026-01-14 14:10:23 +0800

    feat: sast-python2 条件语句、条件表达式和循环结构

--- sast-python2/case/accuracy/path_sensitive/loop_conditional_stmt/solver/while_body_solver_002_T.py
+++ sast-python2/case/accuracy/path_sensitive/loop_conditional_stmt/solver/while_body_solver_002_T.py
@@ -11,11 +11,11 @@ import os
 
 
 def while_body_solver_002_T(taint_src):
-    i = 7
-    res = taint_src
+    i = 0
+    res = "safe_value"
     while i < 3:
-        res = u""
-        break
+        res = taint_src
+        i = i + 1
     else:
         # 当while循环条件不满足时执行
         taint_sink(res)
